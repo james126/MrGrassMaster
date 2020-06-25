ef8e4880086dd530f5bfb03bce8171735ab7c101
James Eason <mrjameseason@gmail.com>
2020-06-26 10:51:25 +1200

    Update index.php

--- mgm/public/index.php
+++ mgm/public/index.php
@@ -13,12 +13,19 @@ $message = 'lawn mowing';
 
 $connnection = pg_connect("host=ec2-34-197-141-7.compute-1.amazonaws.com dbname=dduft7k2jshmsq port=5432 user=mqoerrbfbszdsl password=792c74a4c6579cd51fab1c144328cd879717b8581707f957b33c0f9035c7c590 sslmode=require");
 
-echo "host=ec2-34-197-141-7.compute-1.amazonaws.com dbname=dduft7k2jshmsq port=5432 user=mqoerrbfbszdsl password=792c74a4c6579cd51fab1c144328cd879717b8581707f957b33c0f9035c7c590 sslmode=require";
+//echo "host=ec2-34-197-141-7.compute-1.amazonaws.com dbname=dduft7k2jshmsq port=5432 user=mqoerrbfbszdsl password=792c74a4c6579cd51fab1c144328cd879717b8581707f957b33c0f9035c7c590 sslmode=require";
 
-if ($connection){
+//if ($connection){
     //echo "connection successful\n";
-} else {
+//} else {
     //echo pg_last_error($connection);
+//}
+
+$query = "SELECT * FROM messages";
+$result = pg_query($connection, $query);
+
+while ($row = pg_fetch_assoc($result)) {
+    echo $row['first_name'];
 }
 
 $query = "INSERT INTO messages (first_name, last_name, email, phone, address_line1, address_line2, message) "
