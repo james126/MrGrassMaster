bdc1f0f2f19d0ecf67c750fe778ab7a3f3d88aca
James Eason <mrjameseason@gmail.com>
2020-06-25 18:55:46 +1200

    n/a

--- mgm/public/index.php
+++ mgm/public/index.php
@@ -3,33 +3,37 @@
 <body>
 
 <?php
-$curl = curl_init();
-
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://be.trustifi.com/api/i/v1/email",
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 0,
-      CURLOPT_FOLLOWLOCATION => true,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "POST",
-      CURLOPT_POSTFIELDS =>"{\n  \"recipients\": [{\"email\": \"mrjameseason@gmail.com\", \"name\": \"James Eason\",
-          \"phone\":{\"country_code\":\"+64\",\"phone_number\":\"0211397315\"}}],\n  \"lists\": [],
-          \n  \"contacts\": [],\n  \"attachments\": [],\n  \"title\": \"Title\",\n  \"html\": \"Body\",
-          \n  \"methods\": { \n    \"postmark\": false,\n    \"secureSend\": false,\n    \"encryptContent\": false,
-          \n    \"secureReply\": false \n  }\n}",
-      CURLOPT_HTTPHEADER => array(
-          'x-trustifi-key' => '{{fca0f137011d6ec201ee63ec0f494310c664f2cdcec6e81f}}',
-            'x-trustifi-secret' => '{{a2e9724da357b0661e675da068b92529}}',
-            'Content-Type' => 'application/json'
-      ),
+    $client = new http\Client;
+    $request = new http\Client\Request;
+    $request->setRequestUrl('https://be.trustifi.com/api/i/v1/email');
+    $request->setRequestMethod('POST');
+    $body = new http\Message\Body;
+    $body->append('{
+      "recipients": [{"email": "mrjameseason@gmail.com", "name": "test", "phone":{"country_code":"+1","phone_number":"1111111111"}}],
+      "lists": [],
+      "contacts": [],
+      "attachments": [],
+      "title": "Title",
+      "html": "Body",
+      "methods": {
+        "postmark": false,
+        "secureSend": false,
+        "encryptContent": false,
+        "secureReply": false
+      }
+    }');
+    $request->setBody($body);
+    $request->setOptions(array());
+    $request->setHeaders(array(
+      'x-trustifi-key' => '{{fca0f137011d6ec201ee63ec0f494310c664f2cdcec6e81f}}',
+      'x-trustifi-secret' => '{{a2e9724da357b0661e675da068b92529}}',
+      'Content-Type' => 'application/json'
     ));
+    $client->enqueue($request)->send();
+    $response = $client->getResponse();
+    echo $response->getBody();
 
-    $response = curl_exec($curl);
 
-    curl_close($curl);
-    echo $response;
  ?>
 
 </body>
