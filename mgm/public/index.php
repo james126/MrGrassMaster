a67e092f9436a3cca80abfb20d61c0808ff898ce
James Eason <mrjameseason@gmail.com>
2020-06-25 20:44:18 +1200

    n/a

--- mgm/public/index.php
+++ mgm/public/index.php
@@ -3,35 +3,18 @@
 <body>
 
 <?php
-    $client = new http\Client;
-    $request = new http\Client\Request;
-    $request->setRequestUrl('https://be.trustifi.com/api/i/v1/email');
-    $request->setRequestMethod('POST');
-    $body = new http\Message\Body;
-    $body->append('{
-      "recipients": [{"email": "mrjameseason@gmail.com", "name": "test", "phone":{"country_code":"+1","phone_number":"1111111111"}}],
-      "lists": [],
-      "contacts": [],
-      "attachments": [],
-      "title": "Title",
-      "html": "Body",
-      "methods": {
-        "postmark": false,
-        "secureSend": false,
-        "encryptContent": false,
-        "secureReply": false
-      }
-    }');
-    $request->setBody($body);
-    $request->setOptions(array());
-    $request->setHeaders(array(
-      'x-trustifi-key' => '{{fca0f137011d6ec201ee63ec0f494310c664f2cdcec6e81f}}',
-      'x-trustifi-secret' => '{{a2e9724da357b0661e675da068b92529}}',
-      'Content-Type' => 'application/json'
-    ));
-    $client->enqueue($request)->send();
-    $response = $client->getResponse();
-    echo $response->getBody();
+$first_name = 'First name: ' . $_POST['first_name'];
+$last_name = 'Last name: ' . $_POST['last_name'];
+$email = 'Email: ' . $_POST['email'];
+$phone = 'Phone: ' . $_POST['phone'];
+$address_line1 = 'Number and street: ' . $_POST['address_line1'];
+$address_line2 = 'Suburb: ' . $_POST['address_line2'];
+$message = 'Message: ' . wordwrap($_POST['message'], 50, "\r\n");
+
+$conn = pg_connect(getenv("DATABASE_URL"));
+if ($conn){
+    echo "pass";
+}
 
 
  ?>
