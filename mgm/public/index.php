1276f3c059cd8bf2fab61a9b1d44ef826ef44e46
James Eason <mrjameseason@gmail.com>
2020-06-26 09:31:41 +1200

    Update index.php

--- mgm/public/index.php
+++ mgm/public/index.php
@@ -3,18 +3,22 @@
 <body>
 
 <?php
-$first_name = 'First name: ' . $_POST['first_name'];
-$last_name = 'Last name: ' . $_POST['last_name'];
-$email = 'Email: ' . $_POST['email'];
-$phone = 'Phone: ' . $_POST['phone'];
-$address_line1 = 'Number and street: ' . $_POST['address_line1'];
-$address_line2 = 'Suburb: ' . $_POST['address_line2'];
-$message = 'Message: ' . wordwrap($_POST['message'], 50, "\r\n");
+$first_name = 'billy';
+$last_name = 'brown';
+$email = 'billybrown@gmail.com';
+$phone = '123456';
+$address_line1 = '19 pinero place' ;
+$address_line2 = 'auckland';
+$message = 'lawn mowing';
 
 $connnection = @pg_connect(getenv("DATABASE_URL"));
 $query = "INSERT INTO messages (first_name, last_name, email, phone, address_line1, address_line2, message) "
             . "VALUES ({$first_name}, {$last_name}, {$email}, {$phone}, {$address_line1}, {$address_line2}, {$message});";
 $result = @pg_query($connection, $query);
+
+if ($result){
+    echo 'successful';
+}
 
 
  ?>
