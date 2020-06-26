2bbb98ec743f4a861f642ec1378d81327a522706
James Eason <mrjameseason@gmail.com>
2020-06-26 16:19:46 +1200

    Update Tests.php

--- tests/Tests.php
+++ tests/Tests.php
@@ -2,9 +2,26 @@
 declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
-final class Tests extends TestCase {
-    public function testCanBeUsedAsString(): void {
-        $this->assertEquals('user@example.com','user@example.com');
+final class DatabaseTests extends TestCase {
+    private $connection;
+
+    public function testConnection(): void {
+        $connection = pg_connect(
+            "dbname=dduft7k2jshmsq
+            host=ec2-34-197-141-7.compute-1.amazonaws.com
+            port=5432
+            user=mqoerrbfbszdsl
+            password=792c74a4c6579cd51fab1c144328cd879717b8581707f957b33c0f9035c7c590
+            sslmode=require");
+
+        $this->assertTrue($connection);
     }
+
+    /*public function testInsertInto(): void {
+        $query = "INSERT INTO messages (first_name, last_name, email, phone, address_line1, address_line2, message) "
+                    . "VALUES ('{$first_name}', '{$last_name}', '{$email}', '{$phone}', '{$address_line1}', '{$address_line2}', '{$message}');";
+        $result = pg_query($connection, $query);
+    }*/
+
 }
 ?>
