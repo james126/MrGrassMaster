f436ad8c3579f07b92cd051c3ec0813a38a5dae0
James Eason <mrjameseason@gmail.com>
2020-06-26 16:36:02 +1200

    Update Tests.php

--- tests/Tests.php
+++ tests/Tests.php
@@ -14,7 +14,10 @@ final class DatabaseTests extends TestCase {
             password=792c74a4c6579cd51fab1c144328cd879717b8581707f957b33c0f9035c7c590
             sslmode=require");
 
-        $this->assertTrue($connection);
+        $status = pg_connection_status($connection) ;
+
+
+        $this->assertEquals($status, PGSQL_CONNECTION_OK);
     }
 
     public function testInsertInto(): void {
@@ -30,8 +33,8 @@ final class DatabaseTests extends TestCase {
             . "VALUES ('{$first_name}', '{$last_name}', '{$email}', '{$phone}', '{$address_line1}', '{$address_line2}', '{$message}');";
         $result = pg_query($connection, $query);
 
-        $this->assertTrue($connection);
-        
+        $this->assertNotFalse($connection);
+
     }
 
 }
