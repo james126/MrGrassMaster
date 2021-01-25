5ab6df067280d8fb347fe50a2dca2050c8ca09a9
James <44129935+james126@users.noreply.github.com>
2021-01-26 11:29:26 +1300

    Update footer.php

--- mgm/private/shared/footer.php
+++ mgm/private/shared/footer.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <nav class="navbar sticky-bottom navbar-expand-sm navbar-light bg-light">
         <span class="navbar-text" id="footer-text">
-            &copy; Copyright 2019-2020 KWD. All rights reserved.
+            &copy; Copyright 2021 KWD. All rights reserved.
         </span>
     </nav>
 </body>
